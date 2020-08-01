a0346f01224a1ef0481da431e9288b086e5d30e4
Smile <4213509@qq.com>
2020-08-01 18:05:00 +0800

    更新html，无需发布静态文件，只需在配置开启web访问，即可直接使用

--- publish/swagger.php
+++ publish/swagger.php
@@ -3,11 +3,12 @@
 return [
     'output_file' => BASE_PATH . '/public/swagger.json',
     'swagger' => '2.0',
+    'enable' => false,
+    'host' => 'hyperf.io',
     'info' => [
         'description' => 'hyperf swagger api desc',
         'version' => '1.0.0',
         'title' => 'HYPERF API DOC',
     ],
-    'host' => 'hyperf.io',
     'schemes' => ['http']
 ];
---
+++ src/Swagger.php
@@ -0,0 +1,104 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mzh\Swagger;
+
+use GuzzleHttp\Psr7\Stream;
+use Hyperf\Contract\ConfigInterface;
+use Hyperf\HttpMessage\Stream\SwooleStream;
+use Hyperf\HttpServer\Contract\ResponseInterface;
+use Hyperf\Utils\ApplicationContext;
+use Psr\Http\Message\StreamInterface;
+
+
+/**
+ * @\Hyperf\HttpServer\Annotation\AutoController(prefix="/swagger")
+ * Class SwaggerController
+ */
+class Swagger
+{
+    /**
+     * @var ConfigInterface $config
+     */
+    protected $config;
+
+    public function __construct()
+    {
+        $this->config = ApplicationContext::getContainer()->get(ConfigInterface::class);
+    }
+
+    public function index()
+    {
+        if (!$this->config->get('swagger.enable', false)) {
+            return 'swagger not start';
+        }
+        $domain = $this->config->get('swagger.host', false);
+        $url = $this->config->get('swagger.output_file', false);
+        $url = str_replace(BASE_PATH . '/public', '//' . $domain, $url);
+
+        $res = ApplicationContext::getContainer()->get(ResponseInterface::class);
+        $html = '
+<!-- HTML for static distribution bundle build -->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Swagger UI</title>
+    <link rel="stylesheet" type="text/css" href="https://cdn.staticfile.org/swagger-ui/3.31.1/swagger-ui.min.css" >
+    <link rel="icon" type="image/png" href="https://mail.qq.com/zh_CN/htmledition/images/favicon/qqmail_favicon_48h.png" sizes="32x32" />
+    <style>
+      html
+      {
+        box-sizing: border-box;
+        overflow: -moz-scrollbars-vertical;
+        overflow-y: scroll;
+      }
+
+      *,
+      *:before,
+      *:after
+      {
+        box-sizing: inherit;
+      }
+
+      body
+      {
+        margin:0;
+        background: #fafafa;
+      }
+    </style>
+  </head>
+
+  <body>
+    <div id="swagger-ui"></div>
+        <script src="http://cdn.staticfile.org/swagger-ui/3.31.1/swagger-ui-bundle.min.js"> </script>
+    <script src="http://cdn.staticfile.org/swagger-ui/3.31.1/swagger-ui-standalone-preset.min.js"> </script>
+    <script>
+    window.onload = function() {
+      // Begin Swagger UI call region
+      const ui = SwaggerUIBundle({
+        url: "' . $url . '?t="+(new Date()).getDate(),
+        dom_id: \'#swagger-ui\',
+        deepLinking: true,
+        presets: [
+          SwaggerUIBundle.presets.apis,
+          SwaggerUIStandalonePreset
+        ],
+        plugins: [
+          SwaggerUIBundle.plugins.DownloadUrl
+        ],
+        layout: "StandaloneLayout"
+      })
+      // End Swagger UI call region
+
+      window.ui = ui
+    }
+  </script>
+  </body>
+</html>
+';
+
+        return $res->withBody(new SwooleStream($html))->withHeader('content-type', 'text/html; charset=utf8');
+    }
+}
